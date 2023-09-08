118b2bdf64de2ab9ee7a76cc9caa06bb097c3992
davidtsilva <renato.sb92@gmail.com>
2023-09-08 02:17:21 -0300

    build: first commit

---
+++ src/main/java/com/alura/Alura/AluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.Alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraApplication.class, args);
+	}
+
+}
